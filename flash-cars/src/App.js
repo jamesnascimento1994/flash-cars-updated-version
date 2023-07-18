2ae73e1992ff99828c8338b04e8f748990fb1579
jamesnascimento1994 <james.nascimento94@yahoo.com>
2023-07-18 18:22:49 -0400

    initial commit

--- flash-cars/src/App.js
+++ flash-cars/src/App.js
@@ -4,6 +4,11 @@ function App() {
   return (
     <div className="App">
       <h1>Flash Cars</h1>
+      <button className="start-button">Start Game</button>
+      <div className="score">
+        <aside>Score Right: <span className="score-right">0</span>/10</aside>
+        <aside>Score Wrong: <span className="score-wrong">0</span>/5</aside>
+      </div>
     </div>
   );
 }
